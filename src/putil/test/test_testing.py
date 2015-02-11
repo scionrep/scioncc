c1f485a6ae75684eca422e5a0b2b96ce4ab5d598
Michael Meisinger <michael.meisinger@gmail.com>
2015-02-10 16:48:29 -0800

    Fix tests

--- src/putil/test/test_testing.py
+++ src/putil/test/test_testing.py
@@ -9,7 +9,7 @@ class TestOOIImports(putil.testing.ImportTest):
         # for utilities project only, want to search in BASE/src
         # but this test is in BASE/test/ooi
         # so have to go up two levels, then down to src
-        target_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),'src')
+        target_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),'')
         super(TestOOIImports,self).__init__(target_dir, 'putil', *a, **b)
 
 if __name__ == '__main__':
