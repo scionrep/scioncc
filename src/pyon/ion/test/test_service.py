4502c17bcce0ee15d29a4ce37d53056cef25e7dc
Mykola Kharechko <crchemist@gmail.com>
2015-07-24 11:16:26 +0300

    AG-769: fixed validate_resource_id behaviour.

--- src/pyon/ion/test/test_service.py
+++ src/pyon/ion/test/test_service.py
@@ -2,15 +2,44 @@
 
 __author__ = 'Adam R. Smith'
 
-import pyon
+import uuid
+
+from pyon.core.bootstrap import IonObject
+from pyon.core.exception import NotFound
+from pyon.ion.resource import RT
 from pyon.ion.service import BaseService
 from pyon.util.int_test import IonIntegrationTestCase
 
+
 class TestService(BaseService):
     name = 'test-service'
+    clients = IonObject(RT.Resource)
+
 
 class ServiceTest(IonIntegrationTestCase):
+
+    def setUp(self):
+        self._start_container()
+        self.rr = self.container.resource_registry
+
     def test_serve(self):
         # TODO: Make an equivalent of R1's ServiceProcess
         srv = TestService()
         #srv.serve_forever()
+
+    def test_validate_resource_id(self):
+        srv = TestService()
+        srv.clients.resource_registry = self.rr
+        res_obj1 = IonObject(RT.ActorIdentity)
+
+        newid = uuid.uuid4().hex
+        rid1, _ = self.rr.create(res_obj1, object_id=newid)
+        res_val_obj = srv._validate_resource_id("_id", rid1, optional=True)
+        self.assertTrue(res_val_obj)
+        self.assertRaises(NotFound, srv._validate_resource_id, arg_name="_id",
+                          resource_id="badId",
+                          optional=False)
+        newid2 = uuid.uuid4().hex
+        self.assertIsNone(srv._validate_resource_id("_id", newid2,
+                                                    optional=True))
+        self.rr.delete(rid1)
