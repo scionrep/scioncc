47a891a21b5289b509b71e40bb77f4017cb009c1
Mykola Kharechko <crchemist@gmail.com>
2015-08-26 16:47:44 +0300

    AG-981: Added geom_crosses query support.

--- src/pyon/datastore/postgresql/pg_query.py
+++ src/pyon/datastore/postgresql/pg_query.py
@@ -27,6 +27,7 @@ class PostgresQueryBuilder(object):
               DQ.GOP_CONTAINS_BBOX: "~",
               DQ.GOP_WITHIN_BBOX: "@",
               DQ.GOP_OVERLAPS_GEOM: "ST_Intersects(%s,%s)",
+              DQ.GOP_CROSSES_GEOM: "ST_Crosses(%s,%s)",
               DQ.GOP_CONTAINS_GEOM: "ST_Contains(%s,%s)",
               DQ.GOP_WITHIN_GEOM: "ST_Within(%s,%s)",
               DQ.ROP_OVERLAPS_RANGE: "&&",
